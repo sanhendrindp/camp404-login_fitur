5c60a390f7367e54056c1e5f4247608cc31770c2
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2022-02-25 12:58:55 +0700

    Memodifikasi proses-login.php

--- proses-login.php
+++ proses-login.php
@@ -2,21 +2,27 @@
 $inputEmail = $_POST["email"];
 $inputPassword = $_POST["password"];
 
-$kunciEmail = "sanhendrin.dwip@gmail.com";
-$kunciPassword = "user12345";
+include "koneksi.php";
+$sql = "SELECT * FROM siswa WHERE email = '$inputEmail'";
+$dataSiswa = mysqli_query($connection, $sql);
+$kolom = mysqli_fetch_assoc($dataSiswa);
 
 session_start();
 
-if ($inputEmail == $kunciEmail && $inputPassword == $kunciPassword) {
-    $_SESSION["email"] = $inputEmail;
+if ($inputEmail == $kolom["email"]) {
+    $verifikasiPass = password_verify($inputPassword, $kolom["password"]);
 
-    echo "<script>
-            alert('Login berhasil');
-            window.location.replace('home.php');
-        </script>";
-} else {
-    echo "<script>
-            alert('Login gagal');
-            window.location.replace('login.php');
-        </script>";
+    if ($verifikasiPass == true) {
+        $_SESSION["email"] = $inputEmail;
+
+        echo "<script>
+                alert('Login berhasil');
+                window.location.replace('home.php');
+            </script>";
+    } else {
+        echo "<script>
+                alert('Login gagal');
+                window.location.replace('login.php');
+            </script>";
+    }
 }
